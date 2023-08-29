595b76f2eda30426b9a17c7a455bc53f73c90ff7
Wisdom Eleeza <wisdom.eleeza@amalitech.com>
2023-08-29 13:23:21 +0000

    Working on generating a uniqueID and sending email

--- backend/src/resources/controller/uuid.controller.ts
+++ backend/src/resources/controller/uuid.controller.ts
@@ -1,3 +1,13 @@
-import { v4 as uuidv4 } from 'uuid'
+import { Request, Response } from 'express';
+import { v4 as uuidv4 } from 'uuid';
 
-const uuid = uuidv4()
+const generatedLink = (req: Request, res: Response) => {
+  const generatedUuid = uuidv4();
+
+  // Use backticks (`) to create a template string and interpolate the generatedUuid
+  const link = `https://homepage.com/homepage?uuid=${generatedUuid}`;
+
+  res.status(200).json({ link });
+};
+
+export default generatedLink;
