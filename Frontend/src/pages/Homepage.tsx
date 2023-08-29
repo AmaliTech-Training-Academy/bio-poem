2e4d1d350edfa94289abfbac9b30069d31e7e84f
Gyemfa-Gloria-Abedi <akosuagyemfa98@gmail.com>
2023-08-29 16:29:09 +0000

    worked on the search poem ui

--- Frontend/src/pages/Homepage.tsx
+++ Frontend/src/pages/Homepage.tsx
@@ -3,11 +3,13 @@ import Header from '../components/Header'
 import Carousel from '../components/Carousel'
 import RecentPoems from '../components/RecentPoems'
 import SideBar from '../components/SideBar'
+import SearchPoem from '../components/SearchPoem'
 
 const Homepage = () => {
   return (
     <div className='flex'>
         <SideBar/>
+        <SearchPoem/>
         <div>
         <Header/>
         <Carousel/>
