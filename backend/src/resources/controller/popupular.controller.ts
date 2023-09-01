16fc03d487a02a132c89074fb48d71770741c8b5
Wisdom Eleeza <wisdom.eleeza@amalitech.com>
2023-09-01 11:34:26 +0000

    Working on popular poem  API(2)

--- backend/src/resources/controller/popupular.controller.ts
+++ backend/src/resources/controller/popupular.controller.ts
@@ -1,11 +1,21 @@
 import { Request, Response } from 'express';
 import poem from '../model/create.model';
+import calculatePopularity from '../../utils/calculatePopularity';
 
 const getPopularPoems = async (req: Request, res: Response) => {
   try {
     // Retrieve all poems from the database
-    const poems = await poem.find({}).sort({ popularity: -1 }).limit(10);
-    res.status(200).json(poems);
+    const poems = await poem.find({});
+    
+    const popularPoems = poems.map((p) => ({
+      _id: p._id,
+      popularity: calculatePopularity(p.upvotes, p.downvotes),
+    }));
+
+      // Sort poems by popularity in descending order
+       popularPoems.sort((a, b) => b.popularity - a.popularity);
+
+    res.status(200).json({ success: true, popularPoems });
   } catch (error) {
     console.error('Error fetching popular poems:', error);
     res
