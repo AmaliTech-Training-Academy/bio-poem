bc22d8c00a16560b2e50d646fa8823683efb033c
Benedict Arhin <benedict.arhin@amalitech.com>
2023-09-04 09:12:56 +0000

    Rebasing from production branch

--- Frontend/src/components/Themes.tsx
+++ Frontend/src/components/Themes.tsx
@@ -22,7 +22,7 @@ export const Themes: React.FC<themeProps> = ({currentOption}) => {
     <div className="mb-6 border border-customGrey1 rounded-lg p-4 h-56 lg:w-12/12 xl:w-[480px] flex flex-wrap justify-center gap-3">
         {currentOption === 'solid' ? colors.map((color) => 
         <div 
-          className='w-16 h-14 rounded-lg border border-customGrey1' 
+          className='w-16 h-14 rounded-lg border border-customGrey1 cursor-pointer' 
           style={{background: color.color, border: color.color === selectedTheme ? '1px solid #F06A30' : '1px solid #D1D5DB'}} 
           key={color.id}
           onClick={()=> handleSelectedTheme(color.color)}>
@@ -31,7 +31,7 @@ export const Themes: React.FC<themeProps> = ({currentOption}) => {
         : undefined}
         {currentOption === 'pattern' ? patterns.map((pattern)=> 
         <img 
-          className='w-16 h-14 rounded-lg border border-customGrey1' 
+          className='w-16 h-14 rounded-lg border border-customGrey1 cursor-pointer' 
           src={pattern.pattern} 
           key={pattern.id}
           onClick={()=> handleSelectedTheme(pattern.pattern)}
