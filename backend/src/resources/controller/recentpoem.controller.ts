291e89b99ce641d2941d4d5477cbd477f5fe5651
Wisdom Eleeza <wisdom.eleeza@amalitech.com>
2023-09-05 12:20:59 +0000

    Working Image upload routes(1)

--- backend/src/resources/controller/recentpoem.controller.ts
+++ backend/src/resources/controller/recentpoem.controller.ts
@@ -5,8 +5,11 @@ const recentPoems = async (req: Request, res: Response) => {
   // Default values for page and limit if not provided or not valid numbers
   const currentPage = parseInt(req.query.page as string, 10) || 1;
   const itemsPerPage = parseInt(req.query.limit as string, 10) || 12;
+  console.log(currentPage);
+  console.log(itemsPerPage);
+  
   try {
-    const recentPoem = await poem.find({}).sort({ createdAt: -1 }).limit(15);
+    const recentPoem = await poem.find({}).sort({ createdAt: -1 }).limit(12);
 
     res.status(200).json({ success: true, recentPoems: recentPoem });
   } catch (error) {
