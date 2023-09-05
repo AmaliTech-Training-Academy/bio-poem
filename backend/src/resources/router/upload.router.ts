8824a3df8e04929f00fd3f10192db0d9b6e81e73
Wisdom Eleeza <wisdom.eleeza@amalitech.com>
2023-09-05 15:28:29 +0000

    Working Image upload routes(6)

--- backend/src/resources/router/upload.router.ts
+++ backend/src/resources/router/upload.router.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 import uploadImage from '../../resources/controller/upload.controller';
+import upload from '../../middleware/multerConfig';
 const router = express();
 
-router.patch('/profile-image', uploadImage);
+router.patch('/:username/profile-image', upload.single("image"), uploadImage);
 
 export default router;
--- backend/src/utils/cloudinary.upload.ts
+++ backend/src/utils/cloudinary.upload.ts
@@ -1,29 +1,9 @@
-import cloudinary from 'cloudinary'
+const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.API_KEY,
-    api_secret: process.env.SECRET_KEY,
-  });
-  
-  const cloudinaryUploadImage = async (fileToUploads: string) => {
-    try {
-      return new Promise((resolve, reject) => {
-        cloudinary.uploader.upload(fileToUploads, (result: any, error: any) => {
-          if (error) {
-            reject(error);
-          } else {
-            resolve({
-              url: result.secure_url,
-              asset_id: result.asset_id,
-              public_id: result.public_id,
-            });
-          }
-        }, { resource_type: "auto" });
-      });
-    } catch (error: any) {
-      throw new Error(error.message);
-    }
-  };
-  
-  export default cloudinaryUploadImage;
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET
+});
+
+export default cloudinary;
