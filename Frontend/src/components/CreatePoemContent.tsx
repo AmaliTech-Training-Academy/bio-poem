1909df6dc4a3117212ffd98f535d33e5c0c265ab
Benedict Arhin <benedict.arhin@amalitech.com>
2023-09-06 08:26:56 +0000

    Worked on the post request for the form.

--- Frontend/src/components/CreatePoemContent.tsx
+++ Frontend/src/components/CreatePoemContent.tsx
@@ -3,8 +3,18 @@ import { UserInput } from './UserInput'
 import { useSelector } from 'react-redux'
 import { RootState } from '../store/store'
 
+
 export const CreatePoemContent = () => {
-  const currentPage = useSelector((state:RootState)=> state.form.page)
+  const currentPage = useSelector((state:RootState)=> state.form.page);
+  const container = document.querySelector(".container-stepper")
+  const current = container?.querySelector('[data-title="5"]')
+  console.log(current);
+  
+  
+  if(currentPage === 5){
+    current?.classList.remove('item-step-current');
+    
+  }
 
   return (
     <div className='my-4'>
